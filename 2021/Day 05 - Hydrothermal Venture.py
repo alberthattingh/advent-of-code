560e0cdb3c2fe769b88df2d1ae5600b0fe5688ff
Albert Hattingh <albert.h0504@gmail.com>
2021-12-05 12:51:04 +0200

    2021 - Day 05 - Part 1 refactor

--- 2021/Day 05 - Hydrothermal Venture.py
+++ 2021/Day 05 - Hydrothermal Venture.py
@@ -12,7 +12,8 @@ def readInputData():
 
 
 def plotPoints(lines):
-    points = []
+    points = [[0 for y in range(1000)] for x in range(1000)]
+
     for [start, end] in lines:
         [x1, y1] = start
         [x2, y2] = end
@@ -21,31 +22,13 @@ def plotPoints(lines):
             step = 1 if y1 <= y2 else -1
 
             for y in range(y1, y2+step, step):
-                filteredPoints = list(filter(lambda p: p['x'] == x1 and p['y'] == y, points))
-
-                if len(filteredPoints) > 0:
-                    filteredPoints[0]['count'] += 1
-                else:
-                    points.append({
-                        'x': x1,
-                        'y': y,
-                        'count': 1
-                    })
+                points[x1][y] += 1
 
         elif y1 == y2:
             step = 1 if x1 <= x2 else -1
 
             for x in range(x1, x2+step, step):
-                filteredPoints = list(filter(lambda p: p['x'] == x and p['y'] == y1, points))
-
-                if len(filteredPoints) > 0:
-                    filteredPoints[0]['count'] += 1
-                else:
-                    points.append({
-                        'x': x,
-                        'y': y1,
-                        'count': 1
-                    })
+                points[x][y1] += 1
 
     return points
 
@@ -55,9 +38,10 @@ def s1():
     points = plotPoints(lines)
 
     overlaps = 0
-    for p in points:
-        if p['count'] > 1:
-            overlaps += 1
+    for x in points:
+        for y in x:
+            if y > 1:
+                overlaps += 1
 
     print(f'Solution 1: {overlaps}')
 
